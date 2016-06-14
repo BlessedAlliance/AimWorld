ba4c76a4ca67a192be976399ebcf118966ec595f
gabbydgab <gamba@gabbydgab.com>
2016-06-14 21:54:35 +0800

    fixed pages and configuration

--- config/templates.config.php
+++ config/templates.config.php
@@ -3,16 +3,23 @@
 return [
     // Zend Framework Configuration
     'view_manager' => [
-        'template_map' => [],
+        'template_map' => [
+            'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
+            'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+        ],
         'template_path_stack' => [
             'aim-world' => __DIR__ . '/../templates',
-        ],
+        ]
     ],
 
     // Zend Expressive Configuration
-    'templates'     => [
-        'layout'                    => "site/layout",
-        'map'       => [],
+    'templates'     => [        
+        'map'       => [            
+            'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
+            'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+        ],
         'paths'     => [
             'aim-world'  => [__DIR__ . '/../templates']
         ]
