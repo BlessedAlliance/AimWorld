2ecd0721caa10219898b7569cdd31ec188ce70a5
gabbydgab <gamba@gabbydgab.com>
2016-06-15 09:48:24 +0800

    fixed templates

--- config/templates.config.php
+++ config/templates.config.php
@@ -4,9 +4,12 @@ return [
     // Zend Framework Configuration
     'view_manager' => [
         'template_map' => [
+            'site/layout'               => __DIR__ . '/../templates/layout/default.phtml',
             'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
             'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
             'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+            'template/navigation'       => __DIR__ . '/../templates/layout/template/navigation.phtml',
+            'template/breadcrumbs'      => __DIR__ . '/../templates/layout/template/breadcrumbs.phtml',
         ],
         'template_path_stack' => [
             'aim-world' => __DIR__ . '/../templates',
@@ -16,9 +19,12 @@ return [
     // Zend Expressive Configuration
     'templates'     => [        
         'map'       => [            
+            'site/layout'               => __DIR__ . '/../templates/layout/default.phtml',
             'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
             'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
             'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+            'template/navigation'       => __DIR__ . '/../templates/layout/template/navigation.phtml',
+            'template/breadcrumbs'      => __DIR__ . '/../templates/layout/template/breadcrumbs.phtml',
         ],
         'paths'     => [
             'aim-world'  => [__DIR__ . '/../templates']
